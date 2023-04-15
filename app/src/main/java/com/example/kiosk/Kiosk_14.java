9e1616b4b844d37b5c1bbf9ff16dd7753bede64a
Hoya0718 <munho7823@naver.com>
2023-04-15 17:36:10 +0900

    Finish

---
+++ app/src/main/java/com/example/kiosk/Kiosk_14.java
@@ -0,0 +1,14 @@
+package com.example.kiosk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Kiosk_14 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kiosk14);
+    }
+}

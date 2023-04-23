96b14ffc9a331a5eaa97e876cff1b90d55c64a01
Hoya0718 <munho7823@naver.com>
2023-04-24 01:06:48 +0900

    키오스크 메인~4 인텐트 추가

--- app/src/main/java/com/example/kiosk/Kiosk_6.java
+++ app/src/main/java/com/example/kiosk/Kiosk_6.java
@@ -10,5 +10,7 @@ public class Kiosk_6 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kiosk06);
+
+
     }
 }

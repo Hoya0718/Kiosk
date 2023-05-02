207c1d31e9552b4a4c9f815a1ae0e9903edf33cc
h <equis3351@naver.com>
2023-05-02 12:15:52 +0900

    H intent

--- app/src/main/java/com/example/kiosk/Kiosk_11.java
+++ app/src/main/java/com/example/kiosk/Kiosk_11.java
@@ -2,7 +2,9 @@ package com.example.kiosk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Kiosk_11 extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class Kiosk_11 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kiosk11);
+    }
+
+    public void goto_kiosk_10(View v){
+        Intent goto_kiosk_10 = new Intent(getApplicationContext(),Kiosk_10.class);
+        startActivity(goto_kiosk_10);
+    }
+
+    public void goto_kiosk_12(View v){
+        Intent goto_kiosk_12 = new Intent(getApplicationContext(),Kiosk_12.class);
+        startActivity(goto_kiosk_12);
     }
 }

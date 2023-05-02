2ea328145a65e57d2d706df16b067cdc72b8ab98
h <equis3351@naver.com>
2023-05-02 13:01:54 +0900

    H

---
+++ app/src/main/java/com/example/kiosk/Kiosk_9.java
@@ -0,0 +1,14 @@
+package com.example.kiosk;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Kiosk_9 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kiosk09);
+    }
+}

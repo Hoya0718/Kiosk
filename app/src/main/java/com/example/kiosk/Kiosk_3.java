020e4f8144ad8aa683fd4ac903370c8d36ed229d
Hoya0718 <munho7823@naver.com>
2023-05-02 19:07:03 +0900

    텍스트 변경 완료

--- app/src/main/java/com/example/kiosk/Kiosk_3.java
+++ app/src/main/java/com/example/kiosk/Kiosk_3.java
@@ -4,18 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Locale;
+
 public class Kiosk_3 extends AppCompatActivity {
 
-
+    private TextToSpeech tts;
     private myapp text_size;
     private Button vss;
     private Button vsm;
     private Button vsf;
     private Button pre;
     private Button next;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +38,13 @@ public class Kiosk_3 extends AppCompatActivity {
         pre.setTextSize(text_size.getId());
         next.setTextSize(text_size.getId());
 
+        tts = new TextToSpeech(getApplicationContext(),new TextToSpeech.OnInitListener(){
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    tts.setLanguage(Locale.KOREAN); // TTS 언어 설정
+                }
+            }
+        });
     }
 
     public void goto_kiosk_02(View v){
@@ -44,5 +55,24 @@ public class Kiosk_3 extends AppCompatActivity {
     public void goto_kiosk_04(View v) {
         Intent goto_kiosk_04 = new Intent(getApplicationContext(), Kiosk_4.class);
         startActivity(goto_kiosk_04);
+    }
+    public void volume_speed_slow(View view) {
+        tts.setSpeechRate(0.8f);
+        tts.speak("이 정도 속도 어떠세요?.", TextToSpeech.QUEUE_FLUSH, null,null);
+    }
+    public void volume_speed_medium(View view) {
+        tts.setSpeechRate(1.0f);
+        tts.speak("이 정도 속도 어떠세요?.", TextToSpeech.QUEUE_FLUSH, null,null);
+    }
+    public void volume_speed_fast(View view) {
+        tts.setSpeechRate(1.5f);
+        tts.speak("이 정도 속도 어떠세요?.", TextToSpeech.QUEUE_FLUSH, null,null);
+    }
+    protected void onDestroy() {
+        if(tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
     }
 }

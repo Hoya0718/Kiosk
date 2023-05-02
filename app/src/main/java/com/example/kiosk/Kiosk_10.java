769aa5b71b50ea49056e0060191573976089e0ab
h <equis3351@naver.com>
2023-05-02 18:21:22 +0900

    H

--- app/src/main/java/com/example/kiosk/Kiosk_10.java
+++ app/src/main/java/com/example/kiosk/Kiosk_10.java
@@ -2,7 +2,9 @@ package com.example.kiosk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Kiosk_10 extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class Kiosk_10 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kiosk10);
+    }
+
+    public void goto_kiosk_05(View v){
+        Intent goto_kiosk_05 = new Intent(getApplicationContext(),Kiosk_5.class);
+        startActivity(goto_kiosk_05);
+    }
+
+    public void goto_kiosk_11(View v){
+        Intent goto_kiosk_11 = new Intent(getApplicationContext(),Kiosk_11.class);
+        startActivity(goto_kiosk_11);
     }
 }

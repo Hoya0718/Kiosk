9a2e99ddfb8689a2a3402587ca265b14f32d1c2b
Hoya0718 <munho7823@naver.com>
2023-05-02 19:08:16 +0900

    텍스트 변경 완료

---
+++ app/src/main/java/com/example/kiosk/Kiosk_9.java
@@ -0,0 +1,13 @@
+package com.example.kiosk;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Kiosk_9 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kiosk09);
+    }
+}

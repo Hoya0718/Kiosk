95e3acb8fd3c93a68f563e128da107586a109f21
Hoya0718 <munho7823@naver.com>
2023-05-02 21:53:02 +0900

    유후 유대인

--- app/src/main/java/com/example/kiosk/Kiosk_5.java
+++ app/src/main/java/com/example/kiosk/Kiosk_5.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Locale;
+
 public class Kiosk_5 extends AppCompatActivity {
 
+    private TextToSpeech tts;
+
+    private myapp sound;
     private myapp text_size;
     private Button fastfood;
     private Button bus;
@@ -18,7 +25,7 @@ public class Kiosk_5 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kiosk05);
-        text_size =(myapp)getApplication();
+        text_size = (myapp) getApplication();
         fastfood = findViewById(R.id.fastfood_btn);
         bus = findViewById(R.id.bus_btn);
         hospital = findViewById(R.id.hospital_btn);
@@ -26,13 +33,46 @@ public class Kiosk_5 extends AppCompatActivity {
         fastfood.setTextSize(text_size.getId());
         bus.setTextSize(text_size.getId());
         hospital.setTextSize(text_size.getId());
+
+        sound = (myapp) getApplication();
+
+        tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    tts.setLanguage(Locale.KOREAN); // TTS 언어 설정
+                }
+            }
+        });
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                tts.setSpeechRate(sound.getTtsSpeed()) ;
+                sound.getTtsVolume();
+                tts.speak("음식점 버튼을 눌러주세요.", TextToSpeech.QUEUE_FLUSH, null, null);
+            }
+        }, 3000); // 3초 딜레이
     }
     public void goto_kiosk_06(View v) {
+        if(tts != null) {
+            tts.stop();
+            tts.shutdown();
+            tts=null;
+        }
+        super.onDestroy();
         Intent goto_kiosk_06 = new Intent(getApplicationContext(), Kiosk_6.class);
         startActivity(goto_kiosk_06);
     }
     public void goto_kiosk_25(View v){
         Intent goto_kiosk_25 = new Intent(getApplicationContext(),Kiosk_25.class);
         startActivity(goto_kiosk_25);
+    }
+    protected void onDestroy() {
+        if(tts != null) {
+            tts.stop();
+            tts.shutdown();
+            tts=null;
+        }
+        super.onDestroy();
     }
 }

ba78564a9326dad7580fdc3bc104c855237aec35
김상연 <kimsag@>
2023-05-02 21:36:50 +0900

    데이터 전달

--- app/src/main/java/com/example/kiosk/Kiosk_14.java
+++ app/src/main/java/com/example/kiosk/Kiosk_14.java
@@ -6,15 +6,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 
 public class Kiosk_14 extends AppCompatActivity {
 
     private Button button9; //버스 버튼
+    private SimpleDateFormat mFormat = new SimpleDateFormat("yyyy/M/d");
+    TextView textView11;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kiosk14);
+
+        textView11 = (TextView)findViewById(R.id.textView11);
+
+        Calendar c = Calendar.getInstance();
+
+        SimpleDateFormat format;
+
+        format = new SimpleDateFormat("yyyy/MM/dd-HH:mm:ss");
+        textView11.setText(format.format(c.getTime()));
 
         button9 = findViewById(R.id.button9);
         button9.setOnClickListener(new View.OnClickListener() {

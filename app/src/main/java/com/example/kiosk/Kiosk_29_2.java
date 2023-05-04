3587950220a45a062c36b8adb935a4da74a68a2e
Hoya0718 <munho7823@naver.com>
2023-05-04 21:29:49 +0900

    유후 유대인

--- app/src/main/java/com/example/kiosk/Kiosk_29_2.java
+++ app/src/main/java/com/example/kiosk/Kiosk_29_2.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -16,6 +18,10 @@ public class Kiosk_29_2 extends AppCompatActivity {
     private TextView pnpn;
     private TextView department_2;
     private TextView treatment_day;
+
+    private CheckBox check_Money;
+
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,12 +31,17 @@ public class Kiosk_29_2 extends AppCompatActivity {
         pnpn = findViewById(R.id.pnpn);
         treatment_day = findViewById(R.id.treatment_day);
         department_2.setText(pn.getDepartment());
-        treatment_day.setText(""+pn.getDay());
         pnpn.setText(pn.getGet_pn_2().substring(0,8));
-
+        String formattedTime = sdf.format(new Date(pn.getDay()));
+        treatment_day.setText(formattedTime);
+        check_Money = findViewById(R.id.check_Money);
     }
     public void goto_kiosk_30(View v){
-        Intent goto_kiosk_30 = new Intent(this, Kiosk_30.class);
-        startActivity(goto_kiosk_30);
+        if(check_Money.isChecked()) {
+            Intent goto_kiosk_30 = new Intent(this, Kiosk_30.class);
+            startActivity(goto_kiosk_30);
+        }
+        else
+            Toast.makeText(getApplicationContext(), "체크박스를 확인해주세요", Toast.LENGTH_LONG).show();
     }
 }

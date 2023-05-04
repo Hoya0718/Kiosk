af36f82ce080cb0241bb8fb549a1aa9124b10aff
Hoya0718 <munho7823@naver.com>
2023-05-04 19:32:09 +0900

    유후 유대인

--- app/src/main/java/com/example/kiosk/Kiosk_30_2.java
+++ app/src/main/java/com/example/kiosk/Kiosk_30_2.java
@@ -2,7 +2,9 @@ package com.example.kiosk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Kiosk_30_2 extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class Kiosk_30_2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kiosk30_2);
+    }
+    public void goto_kiosk_31(View v){
+        Intent goto_kiosk_31 = new Intent(this, Kiosk_31.class);
+        startActivity(goto_kiosk_31);
     }
 }

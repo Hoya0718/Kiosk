fafd7b439ec0f03746c757618e7a22a0446d9b33
h <equis3351@naver.com>
2023-05-31 20:22:50 +0900

    패스트푸드 실전 결제 TTS 삭제

--- app/src/main/java/com/example/kiosk/Kiosk_R_Fastfood_Pay.java
+++ app/src/main/java/com/example/kiosk/Kiosk_R_Fastfood_Pay.java
@@ -14,30 +14,12 @@ import java.util.Locale;
 
 public class Kiosk_R_Fastfood_Pay extends AppCompatActivity {
 
-    private TextToSpeech tts;
-    private myapp sound;
-
     private int value;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kiosk_r_fastfood_pay);
-
-        sound = (myapp) getApplication();
-
-        tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
-            public void onInit(int status) {
-                if (getResources().getConfiguration().locale.getLanguage().equals("ko")) {
-                    tts.setLanguage(Locale.KOREAN); // TTS 언어 설정
-                    speakText("카드를 그림과 같이 꽂아주세요.");
-                }
-                else {
-                    tts.setLanguage(Locale.ENGLISH); // TTS 언어 설정
-                    speakText("Insert the card as shown in the picture.");
-                }
-            }
-        });
 
         Intent intent = getIntent();
         if (intent != null) {
@@ -46,7 +28,6 @@ public class Kiosk_R_Fastfood_Pay extends AppCompatActivity {
     }
 
     public void goto_Kiosk_R_H_C (View v){
-        tts.shutdown();
         Intent goto_Kiosk_R_H_C = new Intent(getApplicationContext(), Kiosk_R_Congratulations.class);
 
         myapp orderHistory = (myapp) getApplicationContext();
@@ -56,33 +37,9 @@ public class Kiosk_R_Fastfood_Pay extends AppCompatActivity {
     }
 
     public void goto_Kiosk_R_F_P_M(View v){
-        tts.shutdown();
         Intent goto_Kiosk_R_F_P_M = new Intent(getApplicationContext(),Kiosk_R_Fastfood_Pay_Meth.class);
         goto_Kiosk_R_F_P_M.putExtra("value", value);
         startActivity(goto_Kiosk_R_F_P_M);
-    }
-
-    private void speakText(String text) {
-        tts.setSpeechRate(sound.getTtsSpeed()) ;
-        sound.getTtsVolume();
-        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
-    }
-
-    protected void onDestroy() {
-        if(tts != null) {
-            tts.stop();
-            tts.shutdown();
-            tts=null;
-        }
-        super.onDestroy();
-    }
-
-    protected void onPause() {
-        if (tts != null) {
-            // TTS 발화 중지
-            tts.stop();
-        }
-        super.onPause();
     }
 
     public void onBackPressed() {

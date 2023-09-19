6e689d401b31585460affb8b244496ba384fdcf3
munho7823 <munho7823@naver.com>
2023-09-19 20:03:16 +0900

    추가완료

--- app/src/main/java/com/example/kiosk/Kiosk_R_Fastfood_Congratulation.java
+++ app/src/main/java/com/example/kiosk/Kiosk_R_Fastfood_Congratulation.java
@@ -1,14 +1,18 @@
 package com.example.kiosk;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,18 +21,25 @@ public class Kiosk_R_Fastfood_Congratulation extends AppCompatActivity {
     RoomDB database;
     MainAdapter adapter;
     private MainDao mainDao;
-    private TextView con_text;
+    private TextView con_text, tot, tot_1, tot_2;
+    private String mT, pT_1, pT_2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kiosk_r_fastfood_congratulation);
 
+        tot = findViewById(R.id.tot);
+        tot_1 = findViewById(R.id.tot_1);
+        tot_2 = findViewById(R.id.tot_2);
         database = RoomDB.getInstance(this);
 
         dataList = database.mainDao().getAll();
 
+
+
         adapter = new MainAdapter(Kiosk_R_Fastfood_Congratulation.this, dataList);
+
 
 
 
@@ -56,6 +67,17 @@ public class Kiosk_R_Fastfood_Congratulation extends AppCompatActivity {
         myApp.set_meansTime(payTime);
         myApp.set_meansTime(popTime);
         myApp.set_meansTime(menuTime);
+
+        double md = (menuTime / 60);
+        double mp = (menuTime % 60);
+        double pd = (payTime / 60);
+        double pp = (payTime % 60);
+        double dd = (popTime / 60);
+        double dp = (popTime % 60);
+
+        mT = Double.toString(menuTime);
+        pT_1 = Double.toString(popTime);
+        pT_2 = Double.toString(payTime);
 
         List<Order> orderList = myApp.getOrderList();
         String checkMission = myApp.getCheckFastfoodMission();
@@ -102,19 +124,37 @@ public class Kiosk_R_Fastfood_Congratulation extends AppCompatActivity {
             if (measTime < myApp.getR_F_Time()){
                 myApp.setR_F_Time(measTime);
             }
-        } else {
+        }
+        else
+        {
             con_text.setText("소요 시간 : " + (measTime / 60) + "분 " + (measTime % 60) + "초\n" +
                     "구간별 소요 시간\n" +
-                    "메뉴 선택 : " + (menuTime / 60) + "분 " + (menuTime % 60) + "초\n" +
-                    "세부 선택 : " + (popTime / 60) + "분 " + (popTime % 60) + "초\n" +
-                    "결제 : " + (payTime / 60) + "분 " + (payTime % 60) + "초");
-            data.setTime((menuTime / 60) + "분 " + (menuTime % 60) + "초");
-            data.setDetail((popTime / 60) + "분 " + (popTime % 60) + "초");
-            data.setCredit((payTime / 60) + "분 " + (payTime % 60) + "초");
+                    "메뉴 선택 : " + md + "분 " + mp + "초\n" +
+                    "세부 선택 : " + dd + "분 " + dp + "초\n" +
+                    "결제 선택 : " + pd + "분 " + pp + "초");
+            data.setTime(md + "분 " + mp + "초");
+            data.setDetail(dd + "분 " + dp + "초");
+            data.setCredit(pd + "분 " + pp + "초");
             database.mainDao().insert(data);
-            database.mainDao().update2(myId,(menuTime / 60) + "분 " + (menuTime % 60) + "초\n");
-            database.mainDao().update3(myId,(popTime / 60) + "분 " + (popTime % 60) + "초\n");
-            database.mainDao().update4(myId,(payTime / 60) + "분 " + (payTime % 60) + "초\n");
+
+
+            database.mainDao().update2(myId,mT);
+            database.mainDao().update3(myId,pT_1);
+            database.mainDao().update4(myId,pT_2);
+
+            List<MainData> dataList = database.mainDao().getAllDataWithTime_1();
+
+            Log.d("DataList", "DataList size: " + dataList.size());
+            double averageValue_1 = calculateAverageValue_1(dataList);
+            double averageValue_2 = calculateAverageValue_2(dataList);
+            double averageValue_3 = calculateAverageValue_3(dataList);
+            DecimalFormat decimalFormat = new DecimalFormat("#.#");
+            String formattedAverage_1 = decimalFormat.format(averageValue_1);
+            String formattedAverage_2 = decimalFormat.format(averageValue_2);
+            String formattedAverage_3 = decimalFormat.format(averageValue_3);
+            tot.setText("메뉴 선택 평균값: " + formattedAverage_1 + "초");
+            tot_1.setText("세부 선택 평균값: " +formattedAverage_2 + "초");
+            tot_2.setText("결제 선택 평균값: " +formattedAverage_3 + "초");
             database.mainDao().deleteNullNameData();
             myApp.setR_F_Time(measTime);
         }
@@ -124,17 +164,192 @@ public class Kiosk_R_Fastfood_Congratulation extends AppCompatActivity {
             String appendedText = currentText + "\n임무 성공 여부 : " + missionComplete;
             con_text.setText(appendedText);
         }
-
     }
 
     public void goto_Kiosk_R_P(View v){
-        Intent goto_Kiosk_R_P = new Intent(getApplicationContext(), Kiosk_R_Part.class);
 
-        myapp myApp = (myapp) getApplicationContext();
-        myApp.clearOrderList();
-        myApp.setCheckFastfoodMission("X");
-        myApp.setMissionCheck(false);
+        double tot_value;
+        double tot_1_value;
+        double tot_2_value;
+        tot_value = Double.parseDouble(tot.getText().toString().replace("메뉴 선택 평균값: ","").replace("초",""));
+        tot_1_value = Double.parseDouble(tot_1.getText().toString().replace("세부 선택 평균값: ","").replace("초",""));
+        tot_2_value = Double.parseDouble(tot_2.getText().toString().replace("결제 선택 평균값: ","").replace("초",""));
 
-        startActivity(goto_Kiosk_R_P);
+        double mT_1;
+        double pT_1_1;
+        double pT_2_1;
+
+
+        mT_1 = Double.parseDouble(mT);
+        pT_1_1 = Double.parseDouble(pT_1);
+        pT_2_1 = Double.parseDouble(pT_2);
+
+        double result_1;
+        double result_2;
+        double result_3;
+
+        result_1 = mT_1 - tot_value;
+        result_2 = pT_1_1 - tot_1_value;
+        result_3 = pT_2_1 - tot_2_value;
+
+        AlertDialog.Builder ad = new AlertDialog.Builder(Kiosk_R_Fastfood_Congratulation.this);
+
+        if((result_1 >=result_2) && (result_1 >= result_3)){
+            Log.d("result","result1");
+            ad.setIcon(R.mipmap.ic_launcher);
+            ad.setTitle("소요 시간");
+            ad.setMessage("메뉴 선택시간이 평균값보다 " + result_1 + "초 느려요. 연습을 해 보실까요?\n연습을 하시려면 '네' 필요가 없으시다면 '아니요'를 눌러주세요.");
+            ad.setPositiveButton("네", new DialogInterface.OnClickListener() {            //사용자 이름을 입력시 다음 액티비티로 이동
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent intent = new Intent(getApplicationContext(),Kiosk_2.class);
+                    dialog.dismiss();
+                    startActivity(intent);
+                }
+            });
+            ad.setNegativeButton("아니요", new DialogInterface.OnClickListener() {            //취소시 팝업 종료
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent intent = new Intent(getApplicationContext(),Kiosk_R_Part.class);
+                    dialog.dismiss();
+                    startActivity(intent);
+                }
+            });
+            ad.show();
+        }
+        else if ((result_2 >= result_1) && (result_2 >= result_3)) {
+            Log.d("result","result2");
+            ad.setIcon(R.mipmap.ic_launcher);
+            ad.setTitle("소요 시간");
+            ad.setMessage("세부 선택시간이 평균값보다 " + result_2 +"초 느려요. 연습을 해 보실까요?\n연습을 하시려면 '네' 필요가 없으시다면 '아니요'를 눌러주세요.");
+            ad.setPositiveButton("네", new DialogInterface.OnClickListener() {            //사용자 이름을 입력시 다음 액티비티로 이동
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent intent = new Intent(getApplicationContext(),Kiosk_3.class);
+                    dialog.dismiss();
+                    startActivity(intent);
+                }
+            });
+            ad.setNegativeButton("아니요", new DialogInterface.OnClickListener() {            //취소시 팝업 종료
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent intent = new Intent(getApplicationContext(),Kiosk_R_Part.class);
+                    dialog.dismiss();
+                    startActivity(intent);
+                }
+            });
+            ad.show();
+        }
+        else if((result_3 >= result_1) && (result_3 >= result_2))
+        {
+            Log.d("result","result3");
+            ad.setIcon(R.mipmap.ic_launcher);
+            ad.setTitle("소요 시간");
+            ad.setMessage("결제 선택시간이 평균값보다 " + result_3 + "초 느려요. 연습을 해 보실까요?\n연습을 하시려면 '네' 필요가 없으시다면 '아니요'를 눌러주세요.");
+            ad.setPositiveButton("네", new DialogInterface.OnClickListener() {            //사용자 이름을 입력시 다음 액티비티로 이동
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent intent = new Intent(getApplicationContext(),Kiosk_4.class);
+                    dialog.dismiss();
+                    startActivity(intent);
+                }
+            });
+            ad.setNegativeButton("아니요", new DialogInterface.OnClickListener() {            //취소시 팝업 종료
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent intent = new Intent(getApplicationContext(),Kiosk_R_Part.class);
+                    dialog.dismiss();
+                    startActivity(intent);
+                }
+            });
+            ad.show();
+        }
+
+        else {
+            Intent goto_Kiosk_R_P = new Intent(getApplicationContext(), Kiosk_R_Part.class);
+
+            myapp myApp = (myapp) getApplicationContext();
+            myApp.clearOrderList();
+            myApp.setCheckFastfoodMission("X");
+            myApp.setMissionCheck(false);
+
+            startActivity(goto_Kiosk_R_P);
+        }
+    }
+    private double calculateAverageValue_1(List<MainData> dataList) {
+        // dataList가 null이거나 비어있으면 0을 반환하거나 오류 처리를 수행할 수 있습니다.
+        if (dataList == null || dataList.isEmpty()) {
+            return 0.0;
+        }
+
+
+        double totalValue_1 = 0.0;
+
+        // dataList에서 각 YourEntity 객체의 time 필드 값을 추출하고 더합니다.
+        for (MainData entity : dataList) {
+            String menu_t = entity.getTime();
+
+            if (menu_t != null) {
+                try {
+                    double value_1 = Double.parseDouble(menu_t.replace(" ","").trim());
+                    totalValue_1 += value_1;
+                } catch (NumberFormatException e) {
+                    // time 필드에서 파싱 오류가 발생한 경우 처리
+                    // 예를 들어, "초"를 제거한 후 숫자로 변환할 수 없는 경우
+                    e.printStackTrace(); // 또는 오류 처리 방법을 선택하여 구현
+                }
+            }
+        }
+
+        // 평균 값을 계산하여 반환
+        return totalValue_1 / (dataList.size()-1);
+    }
+    private double calculateAverageValue_2(List<MainData> dataList) {
+        // dataList가 null이거나 비어있으면 0을 반환하거나 오류 처리를 수행할 수 있습니다.
+        if (dataList == null || dataList.isEmpty()) {
+            return 0.0;
+        }
+        double totalValue_2 = 0.0;
+        // dataList에서 각 YourEntity 객체의 time 필드 값을 추출하고 더합니다.
+        for (MainData entity : dataList) {
+            String detail_t = entity.getDetail(); // YourEntity에서 time 필드의 getter 메서드를 사용하도록 수정
+            if (detail_t != null) {
+                try {
+                    double value_2 = Double.parseDouble(detail_t.replace(" ","").trim());
+                    totalValue_2 += value_2;
+                } catch (NumberFormatException e) {
+                    // time 필드에서 파싱 오류가 발생한 경우 처리
+                    // 예를 들어, "초"를 제거한 후 숫자로 변환할 수 없는 경우
+                    e.printStackTrace(); // 또는 오류 처리 방법을 선택하여 구현
+                }
+            }
+        }
+
+        // 평균 값을 계산하여 반환
+        return totalValue_2 / (dataList.size()-1);
+    }
+    private double calculateAverageValue_3(List<MainData> dataList) {
+        // dataList가 null이거나 비어있으면 0을 반환하거나 오류 처리를 수행할 수 있습니다.
+        if (dataList == null || dataList.isEmpty()) {
+            return 0.0;
+        }
+        double totalValue_3 = 0.0;
+        // dataList에서 각 YourEntity 객체의 time 필드 값을 추출하고 더합니다.
+        for (MainData entity : dataList) {
+            String credit_t = entity.getCredit();
+            if (credit_t != null) {
+                try {
+                    double value_3 = Double.parseDouble(credit_t.replace(" ","").trim());
+                    totalValue_3 += value_3;
+                } catch (NumberFormatException e) {
+                    // time 필드에서 파싱 오류가 발생한 경우 처리
+                    // 예를 들어, "초"를 제거한 후 숫자로 변환할 수 없는 경우
+                    e.printStackTrace(); // 또는 오류 처리 방법을 선택하여 구현
+                }
+            }
+        }
+
+        // 평균 값을 계산하여 반환
+        return totalValue_3 / (dataList.size()-1);
     }
 }
--- app/src/main/java/com/example/kiosk/MainDao.java
+++ app/src/main/java/com/example/kiosk/MainDao.java
@@ -1,42 +1,50 @@
-package com.example.kiosk;
+    package com.example.kiosk;
 
-import java.util.List;
-import androidx.room.Dao;
-import androidx.room.Delete;
-import androidx.room.Insert;
-import androidx.room.Query;
+    import java.util.List;
+    import androidx.room.Dao;
+    import androidx.room.Delete;
+    import androidx.room.Insert;
+    import androidx.room.Query;
 
-import java.util.List;
+    import java.util.List;
 
-import static androidx.room.OnConflictStrategy.REPLACE;
-@Dao
-public interface MainDao {
-    @Insert(onConflict = REPLACE)
-    void insert(MainData mainData);
+    import static androidx.room.OnConflictStrategy.REPLACE;
+    @Dao
+    public interface MainDao {
+        @Insert(onConflict = REPLACE)
+        void insert(MainData mainData);
 
-    @Delete
-    void delete(MainData mainData);
+        @Delete
+        void delete(MainData mainData);
 
-    @Delete
-    void reset(List<MainData> mainData);
+        @Delete
+        void reset(List<MainData> mainData);
 
-    @Query("UPDATE table_name SET text = :sText WHERE ID = :sID")
-    void update(int sID, String sText);
+        @Query("UPDATE table_name SET text = :sText WHERE ID = :sID")
+        void update(int sID, String sText);
 
-    @Query("SELECT * FROM table_name")
-    List<MainData> getAll();
+        @Query("SELECT * FROM table_name")
+        List<MainData> getAll();
 
-    @Query("UPDATE table_name SET time = :aaa Where id = :sID")
-    void update2(int sID, String aaa);
-    @Query("UPDATE table_name SET detail = :aaa Where id = :sID")
-    void update3(int sID, String aaa);
-    @Query("UPDATE table_name SET credit = :aaa Where id = :sID")
-    void update4(int sID, String aaa);
+        @Query("UPDATE table_name SET time = :aaa Where id = :sID")
+        void update2(int sID, String aaa);
+        @Query("UPDATE table_name SET detail = :aaa Where id = :sID")
+        void update3(int sID, String aaa);
+        @Query("UPDATE table_name SET credit = :aaa Where id = :sID")
+        void update4(int sID, String aaa);
 
 
-    @Query("DELETE FROM table_name WHERE text IS NULL")
-    void deleteNullNameData();
+        @Query("DELETE FROM table_name WHERE text IS NULL")
+        void deleteNullNameData();
 
-    @Query("SELECT id FROM table_name ORDER BY id DESC LIMIT 1")
-    int getLastInsertedId();
-}
+        @Query("SELECT id FROM table_name ORDER BY id DESC LIMIT 1")
+        int getLastInsertedId();
+
+        @Query("SELECT * FROM table_name WHERE time IS NOT NULL") // "your_table_name"을 실제 테이블 이름으로 변경
+        List<MainData> getAllDataWithTime_1(); // YourEntity는 실제 엔터티 클래스로 변경
+        @Query("SELECT * FROM table_name WHERE detail IS NOT NULL") // "your_table_name"을 실제 테이블 이름으로 변경
+        List<MainData> getAllDataWithTime_2(); // YourEntity는 실제 엔터티 클래스로 변경
+        @Query("SELECT * FROM table_name WHERE credit IS NOT NULL") // "your_table_name"을 실제 테이블 이름으로 변경
+        List<MainData> getAllDataWithTime_3(); // YourEntity는 실제 엔터티 클래스로 변경
+
+    }

e759c8f641bc5de8890cdbb9fb587444165d95e6
H <equis3351@naver.com>
2023-11-20 20:46:22 +0900

    T1 T1 T1

--- app/src/main/java/com/example/kiosk/Kiosk_R_Fastfood_Pay.java
+++ app/src/main/java/com/example/kiosk/Kiosk_R_Fastfood_Pay.java
@@ -1,46 +1,29 @@
 package com.example.kiosk;
 
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.drawable.AnimationDrawable;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.speech.tts.TextToSpeech;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Locale;
-import java.util.UUID;
 
 public class Kiosk_R_Fastfood_Pay extends AppCompatActivity {
 
 
     //블루투스 선언 코드는 여기부터
 
-    private static final String TAG = "BluetoothConnection";
-    private static final String ARDUINO_MAC_ADDRESS = "00:21:09:01:2A:2B"; // 아두이노의 실제 블루투스 맥 주소로 대체해야 합니다.
-    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); // 아두이노와 동일한 UUID 사용
-
-    private Thread workerThread = null; //문자열 수신에 사용되는 쓰레드
-    private byte[] readBuffer; //수신된 문자열 저장 버퍼
-
-    private BluetoothAdapter bluetoothAdapter;
-    private BluetoothDevice arduinoDevice;
-    private int readBufferPosition; //버퍼  내 문자 저장 위치
-    private BluetoothSocket socket;
-    private InputStream inputStream;
-    private OutputStream outputStream;
+//    private static final String TAG = "BluetoothConnection";
+//    private static final String ARDUINO_MAC_ADDRESS = "00:21:09:01:2A:2B"; // 아두이노의 실제 블루투스 맥 주소로 대체해야 합니다.
+//    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); // 아두이노와 동일한 UUID 사용
+//
+//    private Thread workerThread = null; //문자열 수신에 사용되는 쓰레드
+//    private byte[] readBuffer; //수신된 문자열 저장 버퍼
+//
+//    private BluetoothAdapter bluetoothAdapter;
+//    private BluetoothDevice arduinoDevice;
+//    private int readBufferPosition; //버퍼  내 문자 저장 위치
+//    private BluetoothSocket socket;
+//    private InputStream inputStream;
+//    private OutputStream outputStream;
 
     // 여기까지
 
@@ -123,95 +106,96 @@ public class Kiosk_R_Fastfood_Pay extends AppCompatActivity {
 
     //여기도 추가
 
-    private void readDataFromArduino() {
-        final Handler handler = new Handler();
-        // 데이터를 수신하기 위한 버퍼를 생성
-        readBufferPosition = 0;
-        readBuffer = new byte[1024];
-        // 데이터를 수신하기 위한 쓰레드 생성
-        workerThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while(!Thread.currentThread().isInterrupted()) {
-                    try {
-                        // 데이터를 수신했는지 확인합니다.
-                        int byteAvailable = inputStream.available();
-
-                        // 데이터가 수신 된 경우
-                        if(byteAvailable > 0) {
-                            // 입력 스트림에서 바이트 단위로 읽어 옵니다.
-                            byte[] bytes = new byte[byteAvailable];
-                            inputStream.read(bytes);
-                            // 입력 스트림 바이트를 한 바이트씩 읽어 옵니다.
-                            for(int i = 0; i < byteAvailable; i++) {
-                                byte tempByte = bytes[i];
-                                // 개행문자를 기준으로 받음(한줄)
-                                if(tempByte == '\n') {
-                                    // readBuffer 배열을 encodedBytes로 복사
-                                    byte[] encodedBytes = new byte[readBufferPosition];
-                                    System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
-                                    // 인코딩 된 바이트 배열을 문자열로 변환
-                                    final String text = new String(encodedBytes, "UTF-8");
-                                    readBufferPosition = 0;
-                                    handler.post(new Runnable() {
-                                        @Override
-                                        public void run() {
-
-                                            if (text.contains("success")) {
-                                                Intent intent = new Intent(Kiosk_R_Fastfood_Pay.this, Kiosk_R_Fastfood_Congratulation.class); //(해당클래스명.this, 다음 클래스명.class)
-                                                disconnectBluetooth();
-                                                startActivity(intent);
-
-                                            }
-                                        }
-                                    });
-                                } // 개행 문자가 아닐 경우
-                                else {
-                                    readBuffer[readBufferPosition++] = tempByte;
-                                }
-                            }
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    try {
-                        // 1초마다 받아옴
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
-        workerThread.start();
-    }
+//    private void readDataFromArduino() {
+//        final Handler handler = new Handler();
+//        // 데이터를 수신하기 위한 버퍼를 생성
+//        readBufferPosition = 0;
+//        readBuffer = new byte[1024];
+//        // 데이터를 수신하기 위한 쓰레드 생성
+//        workerThread = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                while(!Thread.currentThread().isInterrupted()) {
+//                    try {
+//                        // 데이터를 수신했는지 확인합니다.
+//                        int byteAvailable = inputStream.available();
+//
+//                        // 데이터가 수신 된 경우
+//                        if(byteAvailable > 0) {
+//                            // 입력 스트림에서 바이트 단위로 읽어 옵니다.
+//                            byte[] bytes = new byte[byteAvailable];
+//                            inputStream.read(bytes);
+//                            // 입력 스트림 바이트를 한 바이트씩 읽어 옵니다.
+//                            for(int i = 0; i < byteAvailable; i++) {
+//                                byte tempByte = bytes[i];
+//                                // 개행문자를 기준으로 받음(한줄)
+//                                if(tempByte == '\n') {
+//                                    // readBuffer 배열을 encodedBytes로 복사
+//                                    byte[] encodedBytes = new byte[readBufferPosition];
+//                                    System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+//                                    // 인코딩 된 바이트 배열을 문자열로 변환
+//                                    final String text = new String(encodedBytes, "UTF-8");
+//                                    readBufferPosition = 0;
+//                                    handler.post(new Runnable() {
+//                                        @Override
+//                                        public void run() {
+//
+//                                            if (text.contains("success")) {
+//                                                Intent intent = new Intent(Kiosk_R_Fastfood_Pay.this, Kiosk_R_Fastfood_Congratulation.class); //(해당클래스명.this, 다음 클래스명.class)
+//                                                disconnectBluetooth();
+//                                                startActivity(intent);
+//
+//                                            }
+//                                        }
+//                                    });
+//                                } // 개행 문자가 아닐 경우
+//                                else {
+//                                    readBuffer[readBufferPosition++] = tempByte;
+//                                }
+//                            }
+//                        }
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                    try {
+//                        // 1초마다 받아옴
+//                        Thread.sleep(1000);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }
+//        });
+//        workerThread.start();
+//    }
 
     // onDestroy가 두개라 하나로 합쳐줌
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//
+//        if (socket != null) {
+//            try {
+//                socket.close();
+//            } catch (IOException e) {
+//                Log.e(TAG, "Error occurred while closing the socket: " + e.getMessage());
+//            }
+//        }
+//        super.onDestroy();
+//    }
 
-        if (socket != null) {
-            try {
-                socket.close();
-            } catch (IOException e) {
-                Log.e(TAG, "Error occurred while closing the socket: " + e.getMessage());
-            }
-        }
-        super.onDestroy();
-    }
-    private void disconnectBluetooth() {
-        if (socket != null) {
-            try {
-                workerThread = null;
-                socket.getInputStream().close();
-                socket.getOutputStream().close();
-                socket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+//    private void disconnectBluetooth() {
+//        if (socket != null) {
+//            try {
+//                workerThread = null;
+//                socket.getInputStream().close();
+//                socket.getOutputStream().close();
+//                socket.close();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
 
     // 여기까지
 
